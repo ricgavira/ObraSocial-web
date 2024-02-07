3f681b048808a4a53b85b79097b1575a92323c75
Ricardo Gavira <ricgavira@hotmail.com>
2024-02-07 00:46:59 -0400

    ajustes de layout

--- src/app/components/pessoa-fisica/pessoa-fisica.service.ts
+++ src/app/components/pessoa-fisica/pessoa-fisica.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { IPessoaFisica } from './interface/IPessoaFisica';
 import { environment } from '../../../environments/environment';
@@ -8,7 +8,24 @@ import { PessoaFisicaSimples } from './pessoa-fisica-simples.model';
 @Injectable({
   providedIn: 'root'
 })
+
 export class PessoaFisicaService {
+
+  /**
+  const headers = new HttpHeaders({
+    'Authorization': `Bearer ${localStorage.getItem('token')}`
+  });
+    
+  fazer a autenticacao para os métodos.
+
+
+  - Da pra usar o "Interceptor", pra não ter que ficar passando por parametro toda vez esse headers com autenticacao
+  - Verificar para usar o "Guard" para interceptar rotas, verificar autenticacao e só prosseguir se for permitido
+  - Packagr criar pacote com os compoenntes
+  - NGRX - Gerenciador de Estado (Usuario Logado, dados que precisam transitar entre as páginas, é um localStorage melhorado)
+  - SSR - Server Side (Renderizar pelo servidor e retorna o html pronto)
+
+  */
 
   constructor(private http: HttpClient) { }
 
